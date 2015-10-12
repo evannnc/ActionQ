246f33aab81627d6ada5f76530e326c495d44f20
Evan <evangjones@Evans-Mac-mini.home>
2015-10-11 23:36:37 -0400

    added timeLieftInQ value that tells how much time is left for all of the actions in the queue to finish

--- ActionQ.swift
+++ ActionQ.swift
@@ -1,6 +1,6 @@
 //
-//  actionSerializer.swift
-//  SuperHealos
+//  ActionQ.swift
+//  ActionQ
 //
 //  Created by Evan on 10/5/15.
 //  Copyright (c) 2015 Evan. All rights reserved.
@@ -13,24 +13,26 @@ import SpriteKit
 class Node {
     var action: SKAction? = nil
     var next: Node? = nil
-    var sprite: SKSpriteNode
+    var sprite: SKNode
     
     
-    init(sprite: SKSpriteNode, action: SKAction) {
+    init(sprite: SKNode, action: SKAction) {
         self.action = action
         self.sprite = sprite
     }
 }
 
-
-// the main class that manages the action queue.  simply create an ActionQ object, and call the 
+// the main class that manages the action queue.  simply create an ActionQ object, and call the
 // addNext function to put an action into the queue to be processed immediately.action
 // actions are processed sequentially and each action in the queue waits for the previous action
-// to finish before it will start. 
+// to finish before it will start.
 class ActionQ {
     var count: Int = 0
     var head: Node? = nil
     var tail: Node? = nil
+    
+    // timeLeftInQ is is the number of seconds left for all actions int the queue to finish
+    var timeLeftInQ :NSTimeInterval = 0
     
     init(){
         
@@ -38,9 +40,9 @@ class ActionQ {
     
     
     // add a new sprite/action to the end of the queue to be processed.
-    func addNext(sprite : SKSpriteNode, action :SKAction){
+    func addNext(sprite : SKNode, action :SKAction){
         let n = Node(sprite : sprite, action: action)
-        
+        timeLeftInQ += action.duration
         if self.isEmpty() {
             self.count++
             self.head = n
@@ -57,7 +59,11 @@ class ActionQ {
     
     // this function is called every time an action in the queue is completed
     func actionComplete(){
+        
+        
         if self.head != nil  {
+            // decrement the time left
+            timeLeftInQ -= self.head!.action!.duration
             if self.head!.next != nil {
                 self.head! = self.head!.next!
                 self.head?.sprite.runAction(self.head?.action, completion: self.actionComplete)
